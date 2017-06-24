146dd52fe6da86f0da217268e85a0aa5d7255959
Buddha Shrestha <shrestbuddha@gmail.com>
2017-06-24 22:45:25 +0545

    Update PersonController.groovy

--- grails-app/controllers/grailsangular/PersonController.groovy
+++ grails-app/controllers/grailsangular/PersonController.groovy
@@ -15,34 +15,6 @@ class PersonController {
         Sql sql = new Sql(dataSource)
         def sum = 0;
 
-/*
-            //Async way is depreciated now.
-            def tasks = callTask.collect{z->
-            task{
-                println "Calling Stored Procedure::: " + z + "  Current Thread::" + Thread.currentThread().toString()
-
-                if(z == 1){
-                    def row = sql.rows("{call Proc_1()}");
-                    sum = sum + Integer.parseInt(row[0].get("output").toString());
-                }else if( z == 2){
-                    def row = sql.rows("{call Proc_2()}");
-                    sum = sum + Integer.parseInt(row[0].get("output").toString());
-                }else if( z == 3){
-                    def row = sql.rows("{call Proc_3()}");
-                    sum = sum + Integer.parseInt(row[0].get("output").toString());
-                }else if( z == 4){
-                    def row = sql.rows("{call Proc_4()}");
-                    sum = sum + Integer.parseInt(row[0].get("output").toString());
-                }
-
-                sql.close()
-
-            }
-        }
-        waitAll(tasks)
-*/
-
-
         def row = sql.rows("{call Proc_1()}");
         sum = sum + Integer.parseInt(row[0].get("output").toString());
 
@@ -50,16 +22,5 @@ class PersonController {
         counter++;
         println "Count::: " + counter
         render sum
-    }
-
-    def newPerson(){
-        //form page
-    }
-
-    def save(){
-        println "Parameters::" + params
-        def p = new Person(params)
-        p.save()
-        render "successfully added"
     }
 }
